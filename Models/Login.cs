98e8278100d0d06d533c5df6dc98a61df24587ab
shanyam <shanyam@DESKTOP-SP1A6IF>
2024-09-06 16:53:39 +0530

    Added authorize  by creating jwt tokens and adding it to cookies

--- Models/Login.cs
+++ Models/Login.cs
@@ -4,7 +4,9 @@ namespace BusTicketHub.Models
 {
     public class Login
     {
-        [Required]
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be exactly 10 digits and numeric.")]
+
         public string phone { get; set; }
         [Required]
         public string password { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,40 @@
 using Demo2;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.Cookie.Name = "BusHubCookie";
+    options.Cookie.HttpOnly = true;
+    options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Lax;
+    options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
@@ -14,7 +43,7 @@ app.Use((ctx, next) =>
 {
     var headers = ctx.Response.Headers;
 
-    headers.Add("Developed-by", "OJCommerce");
+    headers.Add("Developed-by", "shanya");
 
     return next();
 });
@@ -37,6 +66,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=HomePage}/{id?}");
+    pattern: "{controller=Home}/{action=Login}/{id?}");
 
 app.Run();
